65b53b78293a96dae50a87877e3057e521252172
Ashish Kumar Ram <74091038+Ashu007A@users.noreply.github.com>
2023-09-26 13:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 //your JS code here. If required.
-
-		$('.card').click(function () {
-            var imgSrc = $(this).find('img').attr('src');
-            $('#modal-image').attr('src', imgSrc);
-        });
